f95769058fc52170cd9ec7f105cb1ecb4c976abb
danielbmancini <danielbmancini@gmail.com>
2023-06-24 05:33:02 -0300

    Updates

---
+++ src/LeetCode150.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public class LeetCode150 {
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            try {
+                int k = Integer.parseInt(token);
+                stack.push(k);
+            } catch (NumberFormatException e) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(operate(token, b, a));
+
+            }
+        }
+        return stack.peek();
+    }
+
+    private static int operate(String token, int a, int b) {
+        switch (token) {
+            case "*":
+                return a * b;
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "/":
+                return a / b;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(evalRPN(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}));
+    }
+}
